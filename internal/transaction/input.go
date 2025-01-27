c3365d4c27a77bbb0b44489e5c267093e8da1c68
Muhammad Sultansyah <muhammadsultansyah14@gmail.com>
2025-01-27 10:35:26 +0700

    feat: add input for transaction

---
+++ internal/transaction/input.go
@@ -0,0 +1,30 @@
+package transaction
+
+type CreateTransactionInput struct {
+	RegisterID      string  `json:"register_id"`
+	RegistrationFee float64 `json:"registration_fee"`
+	ExaminationFee  float64 `json:"examination_fee"`
+	TotalFee        float64 `json:"total_fee"`
+	Discount        float64 `json:"discount"`
+	TotalPayment    float64 `json:"total_payment"`
+	PaymentType     string  `json:"payment_type"`
+	PaymentStatus   string  `json:"payment_status"`
+}
+
+type UpdateTransactionInput struct {
+	RegistrationFee float64 `json:"registration_fee"`
+	ExaminationFee  float64 `json:"examination_fee"`
+	TotalFee        float64 `json:"total_fee"`
+	Discount        float64 `json:"discount"`
+	TotalPayment    float64 `json:"total_payment"`
+	PaymentType     string  `json:"payment_type"`
+	PaymentStatus   string  `json:"payment_status"`
+}
+
+type GetTransactionInput struct {
+	TransactionID int `uri:"transaction_id"`
+}
+
+type GetTransactionByMedicalRecordNoInput struct {
+	MedicalRecordNo int `uri:"medical_record_no"`
+}
